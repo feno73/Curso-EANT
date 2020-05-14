0d7ea56f7c2e820005c31f350b5e782295a69167
feno73 <diego.el.feno@gmail.com>
2020-05-14 20:28:15 -0300

    Arrow functions

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const server = express()
 const docs = express.static("docs")
 server.use(docs)
 
-
+server.get("/ofertas.html", function(request, response){
+    response.end("docs/mercadotech.html")
+})
 server.get("/saludo", function(request, response){
     response.end("docs/mercadotech.html")
 })
---
+++ docs/borrador.js
@@ -0,0 +1,25 @@
+//ES5 Sugar syntax
+function mostrar(){
+
+}
+//ES5 sin sugar
+var mostrar = function(){
+
+}
+//ES6 Arrow function
+const mostrar = () => {
+
+}
+//Equivalente a arrow function
+const mostrar = function(){
+
+}
+
+const Buscar = precio => {
+
+}
+
+const Buscar = (precio, marca) => {
+
+}
+
